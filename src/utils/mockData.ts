8baa24743bbaccbec99614ffa49ee88b844eb543
Aryaman <singh.aryaman24@gmail.com>
2026-01-08 23:38:33 +0530

    mockData/fix

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -9,7 +9,7 @@ export const mockData = [
   {
     id: 1,
     name: "Sri Udupi Park",
-    image: "https://picsum.photos/seed/udupi/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/9/19191079/46078e9cf45c51e23f07576d54c0ec6f.jpg",
     cuisine: ["South Indian", "Vegetarian"],
     rating: 3.9,
     etaMinutes: 25
@@ -17,15 +17,15 @@ export const mockData = [
   {
     id: 2,
     name: "Meghana Foods",
-    image: "https://picsum.photos/seed/meghana/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/chains/1/50691/ecfdc75d21bd7cd9d880f05c8382bc65.jpg",
     cuisine: ["North Indian", "Andhra", "Biryani"],
-    rating: 4.4,
+    rating:4.3 ,
     etaMinutes: 30
   },
   {
     id: 3,
     name: "Empire Restaurant",
-    image: "https://picsum.photos/seed/empire/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/4/50474/fad2d7a11945fb2298048070e6ed9d58_featured_v2.jpg",
     cuisine: ["North Indian", "Chinese", "Mughlai"],
     rating: 3.2,
     etaMinutes: 35
@@ -33,7 +33,7 @@ export const mockData = [
   {
     id: 4,
     name: "Nagarjuna",
-    image: "https://picsum.photos/seed/nagarjuna/300/200",
+    image: "https://images.jdmagicbox.com/v2/comp/bangalore/d7/080pxx80.xx80.231108101822.b9d7/catalogue/nagarjuna-restaurant-whitefield-mahadevapura-bangalore-andhra-restaurants-Wu4L0T6P3m.jpg",
     cuisine: ["Andhra", "South Indian", "Spicy"],
     rating: 4.5,
     etaMinutes: 28
@@ -41,7 +41,7 @@ export const mockData = [
   {
     id: 5,
     name: "Truffles",
-    image: "https://picsum.photos/seed/truffles/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/chains/9/51039/99b002b985def493b463c925ba4c70f6.jpg",
     cuisine: ["American", "Fast Food", "Cafe"],
     rating: 4.3,
     etaMinutes: 22
@@ -49,7 +49,7 @@ export const mockData = [
   {
     id: 6,
     name: "Byg Brewski",
-    image: "https://picsum.photos/seed/brewski/300/200",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAnPMeCSUAHHasZaWW5uXYVlqvfJUTqpLhsQ&s",
     cuisine: ["Continental", "Italian", "Brewery"],
     rating: 2.7,
     etaMinutes: 45
@@ -57,7 +57,7 @@ export const mockData = [
   {
     id: 7,
     name: "MTR 1924",
-    image: "https://picsum.photos/seed/mtr/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/9/54699/5a2931659b2d8d42ed4938ee5b1df02f.jpg",
     cuisine: ["South Indian", "Traditional"],
     rating: 3.6,
     etaMinutes: 20
@@ -65,7 +65,7 @@ export const mockData = [
   {
     id: 8,
     name: "Punjabi Rasoi",
-    image: "https://picsum.photos/seed/punjabi/300/200",
+    image: "https://www.thepunjabirasoi.com/images/logos/image001.png",
     cuisine: ["North Indian", "Punjabi"],
     rating: 4.1,
     etaMinutes: 33
@@ -73,7 +73,7 @@ export const mockData = [
   {
     id: 9,
     name: "Chutney Chang",
-    image: "https://picsum.photos/seed/chutney/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/chains/5/53165/d22582122b353db5b5a9e8e1c5505273_featured_v2.jpg",
     cuisine: ["Chinese", "Asian"],
     rating: 4.0,
     etaMinutes: 40
@@ -81,7 +81,7 @@ export const mockData = [
   {
     id: 10,
     name: "A2B - Adyar Ananda Bhavan",
-    image: "https://picsum.photos/seed/a2b/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/1/50041/7eb6e5197551f38c38116c53126f0965_featured_v2.jpg",
     cuisine: ["South Indian", "Sweets"],
     rating: 4.3,
     etaMinutes: 24
@@ -89,7 +89,7 @@ export const mockData = [
   {
     id: 11,
     name: "Beijing Bites",
-    image: "https://picsum.photos/seed/beijing/300/200",
+    image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/a9/52/ce/caption.jpg?w=900&h=500&s=1",
     cuisine: ["Chinese", "Fast Food"],
     rating: 4.1,
     etaMinutes: 38
@@ -97,7 +97,7 @@ export const mockData = [
   {
     id: 12,
     name: "Sanky's Burger House",
-    image: "https://picsum.photos/seed/burger/300/200",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW48QHlEF8mrrppyDgp4JYhqdR44YFPsof8Q&s",
     cuisine: ["American", "Burger"],
     rating: 4.4,
     etaMinutes: 27
@@ -105,7 +105,7 @@ export const mockData = [
   {
     id: 13,
     name: "The Filter Coffee",
-    image: "https://picsum.photos/seed/coffee/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/1/21359471/6e69ffecc003160a88dc29a4612b0c2a.jpg",
     cuisine: ["South Indian", "Cafe"],
     rating: 4.2,
     etaMinutes: 21
@@ -113,7 +113,7 @@ export const mockData = [
   {
     id: 14,
     name: "Kritunga Restaurant",
-    image: "https://picsum.photos/seed/kritunga/300/200",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiV6L04ctjUGllKmhJc0nlwuxyCO-IdzDmdg&s",
     cuisine: ["Andhra", "Spicy"],
     rating: 4.5,
     etaMinutes: 32
@@ -121,7 +121,7 @@ export const mockData = [
   {
     id: 15,
     name: "Leon Grill",
-    image: "https://picsum.photos/seed/leon/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/chains/1/60551/eefd6011022ffa01ec1c0c9becfaded1_featured_v2.jpg",
     cuisine: ["Fast Food", "Burger"],
     rating: 4.3,
     etaMinutes: 26
@@ -129,7 +129,7 @@ export const mockData = [
   {
     id: 16,
     name: "Toscano",
-    image: "https://picsum.photos/seed/toscano/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/0/21649780/03531993f2feee299871b155792b3e0e_featured_v2.jpg",
     cuisine: ["Italian", "Continental"],
     rating: 4.6,
     etaMinutes: 42
@@ -137,7 +137,7 @@ export const mockData = [
   {
     id: 17,
     name: "Uttam Sagar",
-    image: "https://picsum.photos/seed/uttam/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/2/19844902/e6dfd5d8f4f9e2a8f5c5db854fdd1530.jpg?fit=around|960:500&crop=960:500;*,*",
     cuisine: ["South Indian", "Vegetarian"],
     rating: 4.1,
     etaMinutes: 23
@@ -145,7 +145,7 @@ export const mockData = [
   {
     id: 18,
     name: "Barbeque Nation",
-    image: "https://picsum.photos/seed/bbq/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/2/19241192/ba0ace0f3ce2794fbd54cc762ea3de8f.jpg?fit=around|960:500&crop=960:500;*,*",
     cuisine: ["BBQ", "North Indian"],
     rating: 4.4,
     etaMinutes: 50
@@ -153,7 +153,7 @@ export const mockData = [
   {
     id: 19,
     name: "Rameshwaram Cafe",
-    image: "https://picsum.photos/seed/ramesh/300/200",
+    image: "https://b.zmtcdn.com/data/pictures/2/19911292/8cf509b8019924f642e279e342a5cebc.jpg",
     cuisine: ["South Indian"],
     rating: 4.5,
     etaMinutes: 18
@@ -161,7 +161,7 @@ export const mockData = [
   {
     id: 20,
     name: "Absolute Barbecues",
-    image: "https://picsum.photos/seed/ab/300/200",
+    image: "https://dt4l9bx31tioh.cloudfront.net/eazymedia/group/5743/menu020210928051822.jpg?width=750&height=436&mode=crop",
     cuisine: ["BBQ", "Buffet"],
     rating: 4.6,
     etaMinutes: 48
