652c8262234426aaf1ec7bee90edf76d0f196334
Aryaman <singh.aryaman24@gmail.com>
2026-01-18 15:38:57 +0530

    class_base_state

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,11 @@
+import {useState} from "react"
+
 const UserCard = (props) => {
     const {name, location }= props;
+    const [count]= useState(0);
     return (
         <div className="user-card">
+            <h2>Count: {count}</h2>
             <h2>Name : {name}</h2>
             <h3>Location: {location}</h3>
         </div>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -4,12 +4,17 @@ class UserClass extends React.Component {
 
     constructor(props){
         super(props)
+
+        this.state={
+            count:0
+        }
     }
     render(){
        return  (
        <div className="user-card">
-            <h2>{this.props.name}</h2>
-            <h3>{this.props.location}</h3>
+            <h2>Count : {this.state.count}</h2>
+            <h2>Name : {this.props.name}</h2>
+            <h3>Location : {this.props.location}</h3>
         </div>
        )}
 };
